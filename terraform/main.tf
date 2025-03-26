be044a55b05bf97e419965f0bc0e9ab06137e6c1
Nomad-web-debug <jovenvirus15@gmail.com>
2025-03-26 10:23:05 -0500

    ----------

--- terraform/main.tf
+++ terraform/main.tf
@@ -357,6 +357,7 @@ output "security_group_id" {
 }
 
 output "s3_bucket" {
-  value = aws_s3_bucket.app.id
-  description = "Nombre del bucket S3 creado"
+  value       = aws_s3_bucket.app.id
+  description = "Nombre del bucket S3"
+  depends_on  = [aws_s3_bucket.app]
 } 
